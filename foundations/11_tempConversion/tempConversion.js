f7e1bfbcd97ac985bb964d8fb45cbb8013e1abf6
Vincent <vmgboyles@gmail.com>
2026-04-24 00:59:16 +0800

    finished tempConversion exercises via basic javascript math

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(farenheit) {
+    const celsius = ((farenheit - 32) / 1.8)
+    const round = Math.round(celsius * 10)/10
+    return round
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+    const farenheit = ((celsius * 1.8) + 32)
+    const round = Math.round(farenheit * 10)/10
+    return round
 };
 
 // Do not edit below this line
