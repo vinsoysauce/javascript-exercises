502ae147ede262490843192f58033ea246a9bc0b
Vincent <vmgboyles@gmail.com>
2026-04-24 00:10:05 +0800

    finished sumall exercise via for loop and reduce

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function(a, b) {
+    const arr = []
+    const start = a < b ? a : b
+    const end = a > b ? a : b
+    if (!Number.isInteger(a) || !Number.isInteger(b) || (a < 0 || b < 0)) {
+        return 'ERROR'
+    } 
+    for (let i = start; i <= end; i++) {
+        arr.push(i)
+    }
+    return arr.reduce((initial, current) => initial += current, 0);
 };
 
 // Do not edit below this line
