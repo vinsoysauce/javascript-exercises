80b9066882251c4f5917f45fcad4ad70c5a57af6
Vincent <vmgboyles@gmail.com>
2026-04-23 22:48:35 +0800

    finished removeFromArray exercise with filter and includes array methods

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-};
+const removeFromArray = function(arr, ...remove) {
+    return arr.filter((item) => !remove.includes(item))
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
