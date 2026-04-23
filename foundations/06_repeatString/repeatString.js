4f98d4e17ca66bd2274607db52d2f6cbcacd74ab
Vincent <vmgboyles@gmail.com>
2026-04-23 18:21:27 +0800

    completed 06_repeatingstrings exercise via while loop

--- foundations/06_repeatString/repeatString.js
+++ foundations/06_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let i = 0
+    let arr = []
+    if (num < 0) return 'ERROR'
+    while (i < num) {
+        arr.push(str)
+        i++;
+    }
+    return arr.join('')
 };
 
 // Do not edit below this line
