001e7b74649aec2ebad80928e910a2d1fffba137
Vincent <vmgboyles@gmail.com>
2026-04-23 18:28:37 +0800

    finished 07_reverseString exercise via for loop

--- foundations/07_reverseString/reverseString.js
+++ foundations/07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let newStr = []
+    for(let i = str.length; i >= 0; i--) {
+        newStr.push(str[i])
+    }
+    return newStr.join('')
 };
 
 // Do not edit below this line
